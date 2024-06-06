01fba0b026d50359e0f9128931e7734314ee1ee1
JatinGupta40 <gupta.jatin40@gmail.com>
2024-06-06 14:52:29 +0530

    MultiLevel Form

---
+++ web/modules/custom/multileveform/src/multiLeveForm.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\multileveform\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides a Docvidya Module: Course Integration with DocMod form.
+ */
+class multiLeveForm extends FormBase {
+  
+}
